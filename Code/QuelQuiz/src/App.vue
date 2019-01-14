908db0883312f66d14b70260b9148b3a80301a73
Senistan <senistan.jegarajasingam@cpnv.ch>
2019-01-14 15:18:19 +0100

    New version

--- Code/QuelQuiz/src/App.vue
+++ Code/QuelQuiz/src/App.vue
@@ -1,16 +1,21 @@
 <template>
   <div id="app">
-    <hello></hello>
+    <h1>Un quiz ?</h1>
+    <b-alert show>Une alerte</b-alert>
   </div>
 </template>
-
 <script>
-import Hello from './components/Hello.vue'
-
 export default {
-  name: 'app',
-  components: {
-    Hello
-  }
+  name: 'app'
 }
 </script>
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
