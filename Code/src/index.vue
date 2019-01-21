f57d2cb8ed4ea9ca56fe2a8d34da7b4e8f8a81ee
Senistan <senistan.jegarajasingam@cpnv.ch>
2019-01-21 16:07:31 +0100

    Update index.vue

--- Code/src/index.vue
+++ Code/src/index.vue
@@ -1,30 +1,57 @@
 <template>
   <div id="app">
-    <div id="header">
-      <div class="col-12 col-lg-10">
-        <nav class="navbar navbar-expand-lg navbar-light fixed-top">
-          <div class="container">
-          <a class="navbar-brand" href="#"><img style="width: 150px;" src="./assets/logo.png"></a>
-          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+    <!-- Navigation -->
+    <nav class="navbar navbar-expand-lg navbar-light fixed-top">
+      <div class="container">
+        <a class="navbar-brand" href="#"><img style="width: 100px;" src="./assets/logo.png"></a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
-          </button>
-            <div class="collapse navbar-collapse" id="navbarResponsive">
-              <ul class="navbar-nav ml-auto">
-                  <li class="nav-item"><a class="nav-link" href="#">Connexion</a></li>
-              </ul>
-            </div>
-          </div>
-        </nav>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarResponsive">
+          <ul class="navbar-nav ml-auto">
+              <li class="nav-item"><a class="nav-link" href="#">Connexion</a></li>
+          </ul>
+        </div>
       </div>
-    </div>
-    <div id="main">
-      <h1>QuelQuiz!</h1>
-      <ul id="example-2">
-        <li v-for="quiz of datas.quizzes" :key="quiz.title">
-          {{ quiz.title }}
-        </li>
-      </ul>
-      <b-alert show>Une alerte</b-alert>
+    </nav>
+
+    <!-- Page Content -->
+    <div class="container">
+
+      <!-- Introduction Row -->
+      <h1 class="my-4" style="padding-top: 20px;">
+        Bienvenue sur QuelQuiz ! 
+      </h1>
+      <p>Ce site a été créé en Vue.js !</p>
+
+      <!-- Team Members Row -->
+      <div class="row">
+        <div class="col-lg-12">
+          <h2 class="my-4">Tous les quizz</h2>
+        </div>
+        <div class="col-lg-4 col-sm-6 text-center mb-4">
+          <img class="rounded-circle img-fluid d-block mx-auto" src="http://placehold.it/200x200" alt="">
+          <h3>John Smith
+            <small>Job Title</small>
+          </h3>
+          <p>What does this team member to? Keep it short! This is also a great spot for social links!</p>
+        </div>
+        <div class="col-lg-4 col-sm-6 text-center mb-4">
+          <img class="rounded-circle img-fluid d-block mx-auto" src="http://placehold.it/200x200" alt="">
+          <h3>John Smith
+            <small>Job Title</small>
+          </h3>
+          <p>What does this team member to? Keep it short! This is also a great spot for social links!</p>
+        </div>
+        <div class="col-lg-4 col-sm-6 text-center mb-4">
+          <img class="rounded-circle img-fluid d-block mx-auto" src="http://placehold.it/200x200" alt="">
+          <h3>John Smith
+            <small>Job Title</small>
+          </h3>
+          <p>What does this team member to? Keep it short! This is also a great spot for social links!</p>
+        </div>
+      </div>
+
     </div>
   </div>
 </template>
