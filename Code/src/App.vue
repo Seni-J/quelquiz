212af1af38f0e33a56703eae57865ab012a57adc
Senistan <senistan.jegarajasingam@cpnv.ch>
2019-01-22 15:26:23 +0100

    Corrected version with routing

--- Code/src/App.vue
+++ Code/src/App.vue
@@ -1,6 +1,5 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
     <router-view/>
   </div>
 </template>
@@ -12,12 +11,53 @@ export default {
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  #app {
+    font-family: 'Montserrat', sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    position: absolute;
+  }
+
+
+ .carousel-item {
+    height: 65vh;
+    min-height: 300px;
+    background: no-repeat center center scroll;
+    -webkit-background-size: cover;
+    -moz-background-size: cover;
+    -o-background-size: cover;
+    background-size: cover;
+  }
+
+body {
+  padding-top: 54px;
 }
+
+@media (min-width: 992px) {
+  body {
+    padding-top: 56px;
+  }
+}
+
+#header{
+  display: flex; 
+  align-items: center;
+  justify-content: center;
+  padding: 100px;
+  background-color: darkgrey;
+  width: 57.7%;
+  margin: auto;
+  margin-top: 100px;
+}
+
+.reponse {
+  display: flex; 
+  align-items: center; 
+  justify-content: center; 
+  padding-top: 50px; 
+  padding-bottom: 50px;
+}
+
 </style>
--- Code/src/components/HelloWorld.vue
+++ Code/src/components/HelloWorld.vue
@@ -1,113 +1,65 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li>
-        <a
-          href="https://vuejs.org"
-          target="_blank"
-        >
-          Core Docs
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://forum.vuejs.org"
-          target="_blank"
-        >
-          Forum
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://chat.vuejs.org"
-          target="_blank"
-        >
-          Community Chat
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://twitter.com/vuejs"
-          target="_blank"
-        >
-          Twitter
-        </a>
-      </li>
-      <br>
-      <li>
-        <a
-          href="http://vuejs-templates.github.io/webpack/"
-          target="_blank"
-        >
-          Docs for This Template
-        </a>
-      </li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li>
-        <a
-          href="http://router.vuejs.org/"
-          target="_blank"
-        >
-          vue-router
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vuex.vuejs.org/"
-          target="_blank"
-        >
-          vuex
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vue-loader.vuejs.org/"
-          target="_blank"
-        >
-          vue-loader
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-        >
-          awesome-vue
-        </a>
-      </li>
-    </ul>
+  <div id="quizlist">
+    <!-- Navigation -->
+    <nav class="navbar navbar-expand-lg navbar-light fixed-top">
+      <div class="container">
+        <a class="navbar-brand" href="#"><img style="width: 100px;" src="../assets/logo.png"></a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarResponsive">
+          <ul class="navbar-nav ml-auto">
+              <li class="nav-item"><a class="nav-link" href="#">Connexion</a></li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+    
+    
+    <!-- Page Content -->
+    <div class="container">
+
+      <!-- Introduction Row -->
+      <h1 class="my-4" style="padding-top: 20px;">
+        Bienvenue sur QuelQuiz ! 
+      </h1>
+      <p>Ce site a été créé en Vue.js !</p>
+
+      <!-- Team Members Row -->
+      <div class="row">
+        <div class="col-lg-12">
+          <h2 class="my-4">Tous les quizz</h2>
+        </div>
+        <div v-for="quiz of datas.quizzes" :key="quiz.id">
+              <div class="col-lg-4 col-sm-6 text-center mb-4">
+              <img class="rounded-circle img-fluid d-block mx-auto" src="http://placehold.it/200x200" alt="">
+              <h3><router-link :to="'/' + quiz.id">{{quiz.title}}</router-link>
+              <small>{{quiz.created_by}}</small>
+              </h3>
+              <p>{{quiz.description}}</p>
+          </div>
+        </div>
+      </div>
+
+    </div>
   </div>
+
+
 </template>
 
+
 <script>
+import json from '../api/quizlist.json';
+
 export default {
-  name: 'HelloWorld',
-  data () {
+  name: 'quizlist',
+   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      datas: json
     }
   }
 }
 </script>
+<style>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h1, h2 {
-  font-weight: normal;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
 </style>
