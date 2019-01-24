6f6c69d29cc7caeee83b8714aabeda3a65b831e8
Jérémy <jeremy.gfeller@cpnv.ch>
2019-01-24 10:30:54 +0100

    wip

---
+++ Code/src/components/header.vue
@@ -0,0 +1,30 @@
+/*eslint-disable */
+<template>
+    <!-- Navigation -->
+    <nav class="navbar navbar-expand-lg navbar-black fixed-top">
+      <div class="container">
+        <a class="navbar-brand" href="/"><img style="width: 100px;" src="./assets/logo.png"></a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarResponsive">
+          <ul class="navbar-nav ml-auto">
+            <li class="nav-item"><a class="nav-link" href="/login">Connexion</a></li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+</template>
+
+<script>
+export default {
+     props:{
+        header:{
+            type: Object,
+        }
+    }
+}
+</script>
+<style>
+
+</style>
--- Code/src/components/index.vue
+++ Code/src/components/index.vue
@@ -1,12 +1,12 @@
 
 <template>
   <div>
+    <header/>
     <!-- Page Content -->
     <div class="container">
-
       <div class="row" style="margin-top:40px">
         <div class="col text-center">
-          <h1 style="fontSize:100px">Bienvenue sur QuelQuiz!</h1>
+          <h1 style="fontSize:70px">Bienvenue sur QuelQuiz!</h1>
         </div>
       </div>
       
@@ -15,8 +15,8 @@
         <div class="col-lg-12">
           <h2 class="my-4">Tous les quizz</h2>
         </div>
-        <div v-for="quiz of datas.quizzes" :key="quiz.id">
-          <div class="col-lg-3 col-md-4 col-sm-6 portfolio-item">
+        <div v-for="quiz of datas.quizzes" :key="quiz.id" class="col-lg-3 col-md-4 col-sm-6 portfolio-item">
+          <div>
             <div class="card h-100">
               <img class="card-img-top" src="http://placehold.it/700x400" alt="">
               <div class="card-body">
@@ -37,9 +37,13 @@
 <script>
 
 import json from '../api/quizlist.json'
+import header from './header.vue'
 
 export default {
   name: 'index',
+  components:{
+    header
+  },
   data () {
     return {
       datas: json
