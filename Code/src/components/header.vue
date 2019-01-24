ab9e3a94eb032d631e54291362900264fe18da40
Senistan <senistan.jegarajasingam@cpnv.ch>
2019-01-24 10:41:36 +0100

    logo

--- Code/src/components/header.vue
+++ Code/src/components/header.vue
@@ -1,30 +1,31 @@
 /*eslint-disable */
 <template>
-    <!-- Navigation -->
-    <nav class="navbar navbar-expand-lg navbar-black fixed-top">
-      <div class="container">
+<!-- Navigation -->
+<nav class="navbar navbar-expand-lg navbar-black fixed-top">
+    <div class="container">
         <a class="navbar-brand" href="/"><img style="width: 100px;" src="./assets/logo.png"></a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarResponsive">
-          <ul class="navbar-nav ml-auto">
-            <li class="nav-item"><a class="nav-link" href="/login">Connexion</a></li>
-          </ul>
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item"><a class="nav-link" href="/login">Connexion</a></li>
+            </ul>
         </div>
-      </div>
-    </nav>
+    </div>
+</nav>
 </template>
 
 <script>
 export default {
-     props:{
-        header:{
+    props: {
+        header: {
             type: Object,
         }
     }
 }
 </script>
+
 <style>
 
 </style>
--- Code/src/components/index.vue
+++ Code/src/components/index.vue
@@ -1,72 +1,72 @@
-
 <template>
-  <div>
-    <header/>
+<div>
+
     <!-- Page Content -->
     <div class="container">
-      <div class="row" style="margin-top:40px">
-        <div class="col text-center">
-          <h1 style="fontSize:70px">Bienvenue sur QuelQuiz!</h1>
-        </div>
-      </div>
-      
-      <!-- Team Members Row -->
-      <div class="row">
-        <div class="col-lg-12">
-          <h2 class="my-4">Tous les quizz</h2>
-        </div>
-        <div v-for="quiz of datas.quizzes" :key="quiz.id" class="col-lg-3 col-md-4 col-sm-6 portfolio-item">
-          <div>
-            <div class="card h-100">
-              <img class="card-img-top" src="http://placehold.it/700x400" alt="">
-              <div class="card-body">
-                <h4 class="card-title">
-                  <router-link :to="'/quiz/' + quiz.id">{{quiz.title}}</router-link>
-                </h4>
-                <small>{{quiz.created_by}}</small>
-                <p>{{quiz.description}}</p>
-              </div>
+        <div class="row" style="margin-top:40px">
+            <div class="col text-center">
+                <h1 style="fontSize:70px">Bienvenue sur QuelQuiz!</h1>
             </div>
-          </div>
         </div>
-      </div>
-      
-    </div>
-  </div>
-</template>
-<script>
 
+        <!-- Team Members Row -->
+        <div class="row">
+            <div class="col-lg-12">
+                <h2 class="my-4">Tous les quizz</h2>
+            </div>
+            <div v-for="quiz of datas.quizzes" :key="quiz.id" class="col-lg-3 col-md-4 col-sm-6 portfolio-item">
+                <div>
+                    <div class="card h-100">
+                        <img class="card-img-top" src="http://placehold.it/700x400" alt="">
+                        <div class="card-body">
+                            <h4 class="card-title">
+                                <router-link :to="'/quiz/' + quiz.id">{{quiz.title}}</router-link>
+                            </h4>
+                            <small>{{quiz.created_by}}</small>
+                            <p>{{quiz.description}}</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+</div>
+</template>
+
+<script>
 import json from '../api/quizlist.json'
-import header from './header.vue'
+import Header from './header.vue'
 
 export default {
-  name: 'index',
-  components:{
-    header
-  },
-  data () {
-    return {
-      datas: json
+    name: 'index',
+    components: {
+        Header
+    },
+    data() {
+        return {
+            datas: json
+        }
     }
-  }
 }
 </script>
+
 <style>
 body {
-  padding-top: 54px;
+    padding-top: 54px;
 }
 
 @media (min-width: 992px) {
-  body {
-    padding-top: 56px;
-  }
+    body {
+        padding-top: 56px;
+    }
 }
 
 .portfolio-item {
-  margin-bottom: 30px;
+    margin-bottom: 30px;
 }
 
 .pagination {
-  margin-bottom: 30px;
+    margin-bottom: 30px;
 }
 </style>
