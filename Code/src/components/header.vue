99cc62f03102c4d3297ee1361b02792aff37fe19
Jérémy <jeremy.gfeller@cpnv.ch>
2019-01-26 14:54:06 +0100

    Changer le style de la navbar

--- Code/src/components/header.vue
+++ Code/src/components/header.vue
@@ -1,19 +1,27 @@
 /*eslint-disable */
 <template>
-<!-- Navigation -->
-<nav class="navbar navbar-expand-lg navbar-black fixed-top">
-    <div class="container">
-        <a class="navbar-brand" href="/"><img style="width: 100px;" :src="logo"></a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarResponsive">
-            <ul class="navbar-nav ml-auto">
-                <li class="nav-item"><a class="nav-link" href="/login">Connexion</a></li>
-            </ul>
+    <!-- Navigation -->
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container">
+            <a class="navbar-brand" href="/"><img style="width: 100px;" :src="logo"></a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="/">Accueil <span class="sr-only">(current)</span></a>
+                    </li>
+                </ul>
+            </div>
+            <div class="collapse navbar-collapse" id="navbarResponsive">
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item"><a class="nav-link" href="/login">Connexion</a></li>
+                    <li class="nav-item"><a class="nav-link" href="/register">Inscription</a></li>
+                </ul>
+            </div>
         </div>
-    </div>
-</nav>
+    </nav>
 </template>
 
 <script>
